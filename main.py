439c6f940b66bcdbdf286c16107eb4290b234f82
Pedro Ivo <vegedracp@gmail.com>
2024-03-05 15:51:42 -0300

    agr vai

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ import language
 # *****************************************
 #               egOS: Cybercrime
 # JOGO DE PEDRO IVO, 2024
-# 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
 # *****************************************
 
 # Abre o blt e o configura: se pode ser arrastado, o titulo da janela e de onde vem o input
