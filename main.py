ea3e6326d925c58038a7862029d76b51966ffa1e
vegedra <vegedracp@gmail.com>
2024-03-05 17:12:57 -0300

    arrumei o token

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import language
 # *****************************************
 #               egOS: Cybercrime
 # JOGO DE PEDRO IVO, 2024
-# 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
+# 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
 # *****************************************
 
 # Abre o blt e o configura: se pode ser arrastado, o titulo da janela e de onde vem o input
